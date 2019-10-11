b881e8417248723f227243c7246b0d2ced4e1779
viniblack <51183682+viniblack@users.noreply.github.com>
2019-10-11 12:02:31 -0300

    subindo td o estudo

---
+++ js/calcula_imc.js
@@ -0,0 +1,63 @@
+var titulo = document.querySelector(".titulo");
+titulo.textContent = "Aparecida Nutricionista";
+
+var pacientes = document.querySelectorAll(".paciente");
+
+for(var i = 0; i < pacientes.length ; i++){
+   
+    var paciente = pacientes[i];
+
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
+
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
+
+    var tdImc = paciente.querySelector(".info-imc");
+
+    var pesoEhValido = validaPeso(peso); //tue ou false
+    var alturaEhValida = validaAltura(altura);
+
+    if(!pesoEhValido){
+        console.log("Peso inválido!")
+        pesoEhValido = false;
+        tdImc.textContent = "Peso Inválido"
+        paciente.classList.add("paciente-invalido-peso");
+    }
+
+    if (!alturaEhValida){
+        console.log("Altura Inválida!")
+        alturaEhValida = false;
+        tdImc.textContent = "Altura Inválido";
+        paciente.classList.add("paciente-invalido-altura");
+    }
+
+    if(alturaEhValida && pesoEhValido) {
+        var imc = calculaImc(peso,altura);
+        tdImc.textContent = imc;
+    }
+}
+
+function validaPeso(peso){
+    if (peso >= 0 && peso < 1000){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+function validaAltura (altura){
+    if(altura >= 0 && altura <= 3.0){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+function calculaImc(peso,altura){
+    var imc = 0;
+    imc = peso / (altura*altura);
+
+    return imc.toFixed(2);
+}
+//
